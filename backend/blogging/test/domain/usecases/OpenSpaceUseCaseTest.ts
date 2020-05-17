343a1b5b14697ac44a4b6169efb570206e4abeee
Roman <roman.entwickler@gmail.com>
2020-05-17 09:33:09 +0200

    HCMS-9 - implement tests to open a space

---
+++ backend/blogging/test/domain/usecases/OpenSpaceUseCaseTest.ts
@@ -0,0 +1,72 @@
+import {expect} from 'chai';
+
+const emptyName = '';
+const nameWithOnlyWhitespace = '        ';
+const nameMadeOfOneCharacter = 'a';
+const nameMadeOf50Characters = 'a'.repeat(50);
+const nameMadeOf51Characters = 'a'.repeat(51);
+
+let testSubject: OpenSpaceUseCase;
+
+let idGenerator: IDGenerator = new class implements IDGenerator {
+    generate(): string {
+        return '1';
+    }
+};
+
+beforeEach(() => {
+    testSubject = new OpenSpaceUseCase(new InMemorySpaceRepository(), idGenerator);
+});
+
+describe('Open Space', () => {
+
+    describe('when execute', () => {
+        it('with empty name it fails', () => {
+            const command = new OpenSpaceCommand(emptyName);
+
+            try {
+                testSubject.execute(command)
+            } catch (e) {
+                expect(e.name).to.be.equal('EmptySpaceNameException');
+            }
+        });
+
+        it('with only whitespaces as name it fails', () => {
+            const command = new OpenSpaceCommand(nameWithOnlyWhitespace);
+
+            try {
+                testSubject.execute(command)
+            } catch (e) {
+                expect(e.name).to.be.equal('EmptySpaceNameException');
+            }
+        });
+
+        it('with more than 50 characters as name it fails', () => {
+            const command = new OpenSpaceCommand(nameMadeOf51Characters);
+
+            try {
+                testSubject.execute(command)
+            } catch (e) {
+                expect(e.name).to.be.equal('MoreThan50CharactersSpaceNameException');
+            }
+        });
+
+        it('with one character as name it opens a space', () => {
+            const command = new OpenSpaceCommand(nameMadeOfOneCharacter);
+
+            const openedSpaceEvent = testSubject.execute(command);
+
+            expect(openedSpaceEvent).to.be.equal(new OpenedSpaceEvent(idGenerator.generate(), nameMadeOfOneCharacter))
+        });
+
+        it('with 50 characters as name it opens a space', () => {
+            const command = new OpenSpaceCommand(nameMadeOf50Characters);
+
+            const openedSpaceEvent = testSubject.execute(command);
+
+            expect(openedSpaceEvent).to.be.equal(new OpenedSpaceEvent(idGenerator.generate(), nameMadeOf50Characters))
+        });
+
+    });
+
+});
