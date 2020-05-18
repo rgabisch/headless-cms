b19ab0eefd813f6bd3b3b1c445417a772047d5de
Jan Rückert <janruckert@Jans-MBP.fritz.box>
2020-05-18 13:45:10 +0200

    created space-component + dummy-data

--- frontend/public/audity/src/App.vue
+++ frontend/public/audity/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Space />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Space from './components/Space.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Space
   }
 }
 </script>
---
+++ frontend/public/audity/src/components/Space.vue
@@ -0,0 +1,21 @@
+<template>
+    <ul>
+        <li v-for="space in spaces" :key="space.name">
+            {{ space.name }}
+        </li>
+    </ul>
+</template>
+
+<script>
+
+export default {
+    name: 'Space',
+    data: () => ({
+        spaces: [
+            { name: 'Scrum-Podcast'},
+            { name: 'Mein Reisepodcast'}
+        ]
+    })
+}
+
+</script>
