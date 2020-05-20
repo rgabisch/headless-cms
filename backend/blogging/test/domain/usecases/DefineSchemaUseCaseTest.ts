408a172237272c511c247214bf5029db27748afb
Roman <roman.entwickler@gmail.com>
2020-05-20 16:39:16 +0200

    write test that should fail

---
+++ backend/blogging/test/domain/usecases/DefineSchemaUseCaseTest.ts
@@ -0,0 +1,10 @@
+suite('Define Schema Use Case', () => {
+    suite('when execute', () => {
+
+        test('given empty name -> throw exception for empty name.');
+        test('given name made of whitespaces -> throw exception for empty name.');
+        test('given name with more than 50 characters -> throw exception for to long name.');
+        test('given unassigned creator id -> throw exception for a empty unassigned id.');
+
+    })
+});
