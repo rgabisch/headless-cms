368d2e6b2814c05362809b9d564e8b8ffcbc42a8
Schaeran <s75948@beuth-hochschule.de>
2020-05-25 18:02:27 +0200

    Creator-Repository, small changes to FireBase.ts

---
+++ backend/blogging/src/infastructure/repositories/FireBaseCreatorRepository.ts
@@ -0,0 +1,24 @@
+import {CreatorRepository} from "../../domain/repositories/DefineSchemaUseCase";
+import Creator from "../../domain/entities/Creator";
+import FireBase from './firebase/firebase'
+
+class FireBaseCreatorRepository extends FireBase implements CreatorRepository {
+
+    constructor(){
+        super('Creator')
+    }
+
+    async findBy(id: string): Promise<Creator | undefined> {
+        return super.db_get(id)
+    }
+
+    async add(creator: Creator) {
+        return super.db_add(creator.id, creator)
+    }
+
+    update(creator: Creator): void {
+        super.db_update(creator.id, creator)
+    }
+}
+
+export default FireBaseCreatorRepository;
