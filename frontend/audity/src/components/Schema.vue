f3bccb31d42f36a7b1ef783a78f1e5d4d1f5697a
Anna Shopova <annna95@gmail.com>
2020-05-26 11:36:29 +0200

    added empty col for the NavBar and a delete-container for not wanted types

--- frontend/audity/src/components/Schema.vue
+++ frontend/audity/src/components/Schema.vue
@@ -1,107 +1,85 @@
 <template>
-    <div>
         <v-row>
-            <v-col lg="2">
-                <v-card>
-                    <v-navigation-drawer
-                        v-model="drawer"
-                    >
-                        <v-list>
-                            <v-list-item>
-                                <v-list-item-content>John Doe</v-list-item-content>
-                            </v-list-item>
-                            <v-list-item>
-                                <v-list-item-content>Dashboard</v-list-item-content>
-                            </v-list-item>
-                            <v-list-item>
-                                <v-list-item-content>
-                                    <v-treeview :items="items"></v-treeview>
-                                </v-list-item-content>
-                            </v-list-item>
-                            <v-list-item>
-                                <v-list-item-content>Dateien</v-list-item-content>
-                            </v-list-item>
-                        </v-list>
-                    </v-navigation-drawer>
-                </v-card>
-            </v-col>
-
-            <v-col lg="6" class="mr-2 ml-2">
+            <v-col lg="2"></v-col>
+            <v-col>
                 <h1>Contenttyp erstellen</h1>
                 <p>Zurück</p>
                 <v-form v-model="formValid" ref="form">
-                   <v-text-field
-                        label="Name des Contenttyps"
-                        filled
-                        dense
-                        v-model="name"
-                        :rules="rules.required"
-                   ></v-text-field>
-
-                  <v-card class="p-3 mt-3" >
-                    <v-card-title text-center>Drop here</v-card-title>
-                    <Container
-                        drag-handle-selector=".column-drag-handle"
-                        group-name="draggable"
-                        :get-child-payload="getChildPayloadSelectedTypes"
-                        @drop="onDrop('selectedTypes', $event)">
-                        <Draggable v-for="type in selectedTypes" :key="type.id">
-                          <div class="draggable-item">
-                            <span class="column-drag-handle" style="float:left">&#x2630;</span>
-                            <v-text-field
-                                  placeholder=""
-                                  outlined
-                                  v-model="type.name"
-                                  :rules="rules.required"
-                            ></v-text-field>
-                          </div>
-                        </Draggable>
-                    </Container>
-                  </v-card>
+                    <v-text-field
+                            label="Name des Contenttyps"
+                            filled
+                            dense
+                            v-model="name"
+                            :rules="rules.required"
+                    ></v-text-field>
+                    <v-card class="p-3 mt-3 text-center" >
+                        <v-card-title class="">Drop here</v-card-title>
+                        <Container
+                                class="drop-container"
+                                group-name="draggable"
+                                :get-child-payload="getChildPayloadSelectedTypes"
+                                @drop="onDropCopy('selectedTypes', $event)">
+                            <Draggable v-for="type in selectedTypes" :key="type.id">
+                                <div class="draggable-item pa-2">
+                                    <span class="column-drag-handle">&#8645;</span>
+                                    <v-text-field
+                                            placeholder=""
+                                            outlined
+                                            v-model="type.name"
+                                            :rules="rules.required"
+                                    ></v-text-field>
+                                </div>
+                            </Draggable>
+                        </Container>
+                    </v-card>
+                    <v-card>
+                        <Container
+                                class="delete-container text-center"
+                                group-name="draggable"
+                                @drop="onDropDelete('selectedTypes', $event)"
+                        ><span>&#10006;</span>
+                        </Container>
+                    </v-card>
                 </v-form>
             </v-col>
-
-            <v-col lg="3">
-              <v-card class="p-3">
-                <v-card-title>Aktionen</v-card-title>
-                <v-btn
-                    x-large
-                    block
-                    color="#FF8E3C"
-                    @click="createSchema"
-                >Speichern
-                </v-btn>
-                <v-btn
-                    class="mt-3"
-                    x-large
-                    block
-                    color="#FF8E3C"
-                >Veröffentlichen
-                </v-btn>
-              </v-card>
-
-
-              <v-card class="p-3 mt-3">
-                <v-card-title>Typen</v-card-title>
-                <Container
-                    behaviour="copy"
-                    group-name="draggable"
-                    :get-child-payload="getChildPayloadTypes">
-                    <Draggable v-for="type in types" :key="type.id">
-                        <div class="draggable-item text-center">
-                          <v-alert
-                            outlined
-                            color="#FF8E3C"
-                          >
-                            {{type.name}}
-                          </v-alert>
-                        </div>
-                    </Draggable>
-                </Container>
-              </v-card>
+            <v-col lg="3" class="ml-5">
+                <v-card class="p-3">
+                    <v-card-title>Aktionen</v-card-title>
+                    <v-btn
+                        large
+                        block
+                        color="#FF8E3C"
+                        @click="createSchema"
+                    >Speichern
+                    </v-btn>
+                    <v-btn
+                        class="mt-3"
+                        large
+                        block
+                        color="#FF8E3C"
+                    >Veröffentlichen
+                    </v-btn>
+                </v-card>
+                <v-card class="p-3 mt-3">
+                    <v-card-title>Typen</v-card-title>
+                    <Container
+                            behaviour="copy"
+                            group-name="draggable"
+                            :get-child-payload="getChildPayloadTypes">
+                        <Draggable v-for="type in types" :key="type.id">
+                            <div class="draggable-item text-center">
+                                <v-alert
+                                    outlined
+                                    color="#FF8E3C"
+                                >
+                                    {{type.name}}
+                                </v-alert>
+                            </div>
+                        </Draggable>
+                    </Container>
+                </v-card>
             </v-col>
         </v-row>
-    </div>
 </template>
 
 <script>
@@ -146,9 +124,12 @@ export default {
           }
           return result
       },
-      onDrop (collection, dropResult) {
+      onDropCopy (collection, dropResult) {
           let newObj = JSON.parse(JSON.stringify(dropResult));
           this[collection] = this.applyDrag(this[collection], newObj)
+      },
+      onDropDelete(collection, dropResult) {
+          collection.splice(dropResult, 1)
       },
       getChildPayloadTypes (index) {
           return this.types[index]
@@ -163,13 +144,15 @@ export default {
                   name: this.name,
                   types: this.selectedTypes
               }
-              console.log(JSON.stringify(Schema))
-              axios.post('http://localhost:3000/schemas', JSON.stringify(Schema))
+              axios.post('http://localhost:3000/schemas', Schema)
                       .then((response) => {
                           console.log(response);
                       }, (error) => {
                           console.log(error);
                       });
+          }
+          else {
+              alert('All required fields must be filled!');
           }
       }
   },
@@ -179,3 +162,26 @@ export default {
 }
 
 </script>
+
+<style>
+    .drop-container{
+        min-height: 330px;
+    }
+    .column-drag-handle{
+        float: left;
+        font-size: 2rem;
+        color: #0798bb;
+        cursor: grab;
+    }
+    .delete-container{
+        height: 65px;
+        background: #f0f0f0;
+    }
+    .delete-container>span{
+        font-size: 3rem;
+        color: #ff7354;
+    }
+    .draggable-item{
+        cursor: grab;
+    }
+</style>
