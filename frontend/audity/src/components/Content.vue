d0e7bf81abf88f90593cceff2154a37680071f2e
Anna Shopova <annna95@gmail.com>
2020-05-29 11:59:44 +0200

    created new components AllContents.vue and Content.vue

---
+++ frontend/audity/src/components/Content.vue
@@ -0,0 +1,63 @@
+<template>
+    <v-row>
+        <v-col lg="2"></v-col>
+        <v-col>
+            <h1>{{content.name}}</h1>
+            <p>{{content.schema.name}}</p>
+            <v-card class="p-3 mt-3 text-center">
+                <div class="p-3" v-for="map in content.mapping" :key="map.type.name">
+                    <v-card>
+                        <v-card-subtitle>{{map.type.name}}</v-card-subtitle>
+                        <v-card-text>{{map.content}}</v-card-text>
+                    </v-card>
+                </div>
+            </v-card>
+        </v-col>
+        <v-col lg="3" class="ml-5">
+            <v-card class="p-3">
+                <v-card-title>Aktionen</v-card-title>
+                <v-btn
+                        class="mt-3"
+                        block
+                        color="#FF8E3C"
+                >Veröffentlichen
+                </v-btn>
+            </v-card>
+            <v-card class="p-3 mt-3">
+                {{content}}
+            </v-card>
+        </v-col>
+    </v-row>
+</template>
+
+<script>
+    import axios from "axios";
+
+    export default {
+        name: "Content",
+        props: ['cid'],
+        data: () => ({
+            content: '',
+
+        }),
+        computed: {
+            contentId() {
+                return this.cid
+            }
+        },
+        mounted() {
+            axios
+                .get('http://localhost:3000/contents/'+ this.contentId +'/spaces/a8a42e70-a127-11ea-9054-05581f9d528d',
+                    {headers: {'creatorId':1}})
+                .then(response => (this.content = response.data))
+                .catch(function (error) {
+                    console.log(error);
+                });
+        },
+
+    }
+</script>
+
+<style scoped>
+
+</style>
