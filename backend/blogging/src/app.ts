cb25674be3d306fec7cbea12729c440522bc321a
Anna Shopova <annna95@gmail.com>
2020-05-29 11:51:46 +0200

    added header {'creatorId':1} temporary for testing

--- backend/blogging/src/app.ts
+++ backend/blogging/src/app.ts
@@ -38,7 +38,7 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, creatorId');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     next();
 });
