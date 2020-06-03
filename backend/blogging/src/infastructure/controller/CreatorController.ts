8095c2908c834d8d808653e648fe78b228121db2
Roman <roman.entwickler@gmail.com>
2020-06-03 18:47:49 +0200

    implement rest interace to create a creator

---
+++ backend/blogging/src/infastructure/controller/CreatorController.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import CreateCreatorUseCase from "../../domain/usecases/CreateCreatorUseCase";
+import CreateCreatorCommand from "../../domain/commands/CreateCreatorCommand";
+
+class CreatorController {
+    constructor(private createCreatorUseCase: CreateCreatorUseCase) {
+    }
+
+    routes(): express.Router {
+        const router = express.Router();
+
+        router.post('/', async (req, res) => {
+            const command = new CreateCreatorCommand(req.body.creatorId);
+
+            try {
+                const createdCreatorEvent = await this.createCreatorUseCase.execute(command);
+                res.send(createdCreatorEvent);
+            } catch (e) {
+                res.status(400).send('post body is invalid');
+            }
+        });
+
+        return router;
+    }
+}
+
+export default CreatorController;
