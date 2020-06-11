3ce937d880059fcce8271b3cb28f28594d214c7f
Schaeran <s75948@beuth-hochschule.de>
2020-06-11 11:56:45 +0200

    authorization accepted in the header

--- backend/app.ts
+++ backend/app.ts
@@ -63,7 +63,7 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, creatorId, cryptedJWT');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, creatorId, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     next();
 });
