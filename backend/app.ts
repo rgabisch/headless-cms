29bfbbf04a512bd364b4d9f07c840f4c32f28f28
Roman <roman.entwickler@gmail.com>
2020-06-15 11:35:20 +0200

    add scripts to manage the application at root level; add an instruction to start the application into the readme

--- backend/app.ts
+++ backend/app.ts
@@ -1,9 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser'
 import GlobalUniqueIdGenerator from "./blogging/src/shared/GlobalUniqueIdGenerator";
-import InMemoryCreatorRepository from "./blogging/src/infastructure/repositories/InMemoryCreatorRepository";
-import Creator from "./blogging/src/domain/entities/Creator";
-import Schema, {TypeDefinition} from "./blogging/src/domain/entities/Schema";
 import TypeFactory from "./blogging/src/domain/factories/TypeFactory";
 import CreatorRepositoryFactory from "./blogging/src/infastructure/repositories/CreatorRepositoryFactory";
 import EnvironmentFactory from "./blogging/src/infastructure/environment/EnvironmentFactory";
@@ -16,7 +13,6 @@ import FireBaseUserRepository from "./identifying/src/infastructure/FireBaseUser
 import SignUpUseCase from "./identifying/src/domain/usecases/SignUpUseCase";
 import InMemoryUserRepository from "./identifying/src/infastructure/InMemoryUserRepository";
 import CreateCreatorUseCase from './blogging/src/domain/usecases/CreateCreatorUseCase';
-import {resolveSoa} from 'dns';
 
 
 const app = express();
@@ -39,10 +35,6 @@ const identifyingController = new IdentifyingController(new SignInUseCase(userRe
 
 
 if (process.env.NODE_ENV == Environment.DEV) {
-    // Create a new Creator
-    const one_creator = new Creator('1', new Map(), new Map());
-    (creatorRepository as InMemoryCreatorRepository).add(one_creator);
-
     signUpUseCase.execute({email: 'test@mail.de', pass: 'test'});
 }
 
